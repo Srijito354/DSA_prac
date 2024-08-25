2bcda339c1b5c18cf2516f37ad66f7a472529876
Srijito354 <srijitoghosh113@outlook.com>
2024-08-26 02:01:28 +0530

    First edit

--- sorting_algo/HeapSort.c
+++ sorting_algo/HeapSort.c
@@ -5,14 +5,28 @@ int main()
 {
     int arr[6] = {5, 9, 8, 7, 1, 3};
     int n = sizeof(arr)/sizeof(int);
-    left = arr[0];
-    right = arr[n];
-    pivot = arr[0];
+    int parent = arr[0];
+    int temp, temp2, temp3=0;
+    int max = 0;
+    loop:
+        for (int i = temp3; i<n; i++)
+        {
+            if (arr[i]>max)
+            {
+                max = arr[i];
+                temp = i;
+            }
+        }
+    temp2 = arr[temp3];
+    arr[temp3] = max;
+    arr[temp] = temp2;
+    temp3++;
+    goto
+    //printf("%d", parent);
+
     for (int i = 0; i<n; i++)
     {
-        if (pivot < right)
-        {
-            
-        }
+        printf("%d ", arr[i]);
     }
+    printf("\n");
 }

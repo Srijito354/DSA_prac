eafbffe78dead29054f914922ca4c018d02f2289
Srijito354 <srijitoghosh113@outlook.com>
2024-08-25 08:49:59 +0530

    Existing files and folder commit

---
+++ sorting_algo/HeapSort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[6] = {5, 9, 8, 7, 1, 3};
+    int n = sizeof(arr)/sizeof(int);
+    left = arr[0];
+    right = arr[n];
+    pivot = arr[0];
+    for (int i = 0; i<n; i++)
+    {
+        if (pivot < right)
+        {
+            
+        }
+    }
+}
